a327a319f8a2f9dc00c1658580e6a66dbedae6ba
WM-IL000583 <wm-il000583@Teds-MacBook-Pro13-M2.local>
2024-05-08 16:29:33 +0900

    wip

--- components/windowDialog/index.tsx
+++ components/windowDialog/index.tsx
@@ -1,3 +1,27 @@
 import { createContext } from "react";
 
+/* -------------------------------------------------------------------------------------------------
+ * Window Dialog heavily influenced by Radix-ui
+ * -----------------------------------------------------------------------------------------------*/
+
+const NAME = "WindowDialog";
+
+type WindowDialogElement = React.ElementRef<"div">;
+
+interface WindowDialogProps {
+  /**
+   * The controlled state of the toggle.
+   */
+  pressed?: boolean;
+  /**
+   * The state of the toggle when initially rendered. Use `defaultPressed`
+   * if you do not need to control the state of the toggle.
+   * @defaultValue false
+   */
+  defaultPressed?: boolean;
+  /**
+   * The callback that fires when the state of the toggle changes.
+   */
+  onPressedChange?(pressed: boolean): void;
+}
 const WindowDialogProviderContext = createContext<any>(undefined);
