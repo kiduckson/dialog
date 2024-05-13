3698c17acd09cdfde3503524adc9c99bfdc35d1d
WM-IL000583 <wm-il000583@Teds-MacBook-Pro13-M2.local>
2024-05-13 13:45:34 +0900

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,8 @@ import { useState } from "react";
 import { Book, Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import DialogContainer from "@/components/windowDialog/dialogContainer";
-import { ScopeTest, ScopeTestTrigger } from "@/components/ScopeTest";
-
-import type { ScopeTestProps } from "@/components/ScopeTest";
 
 export default function Home() {
-  const [clicked1, setClicked1] = useState(false);
-  const [clicked2, setClicked2] = useState(false);
-  const [clicked3, setClicked3] = useState(false);
   return (
     <main className="flex min-h-screen min-w-screen flex-col items-center justify-between p-12 bg-background">
       <div className="flex justify-between items-center w-full p-1 bg-background border-2">
@@ -21,18 +15,6 @@ export default function Home() {
         </Button>
       </div>
       <DialogContainer />
-      <ScopeTest>
-        <div>{clicked1 ? "clicked" : "not"}</div>
-        <ScopeTestTrigger onClick={() => setClicked1((prop) => !prop)} />
-      </ScopeTest>
-      <ScopeTest>
-        <div>{clicked2 ? "clicked" : "not"}</div>
-        <ScopeTestTrigger onClick={() => setClicked2((prop) => !prop)} />
-      </ScopeTest>
-      <ScopeTest>
-        <div>{clicked3 ? "clicked" : "not"}</div>
-        <ScopeTestTrigger onClick={() => setClicked3((prop) => !prop)} />
-      </ScopeTest>
     </main>
   );
 }
